43ab62c25ad68de467418c412aa8cf6fc7be594b
hamzaabbasi54 <hamza.abbasi3030@gmail.com>
2025-08-22 09:44:38 -0700

    blank

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ dotenv.config();
 
 import connectDB from "./db/db_connection.js";
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.port || 8000, ()=>{
+        console.log(`Server is running on port ${process.env.port || 8000}`);
+    })
+})
+.catch((error)=>{
+    console.error("Failed to connect to the database:", error);
+})
